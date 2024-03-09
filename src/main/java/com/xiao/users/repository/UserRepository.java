7c82df968a7dfe2f170a129e07169b87a465d900
Danh <tnd.dev99@gmail.com>
2024-03-09 13:58:01 +0700

    remove redundant method

--- src/main/java/com/xiao/users/repository/UserRepository.java
+++ src/main/java/com/xiao/users/repository/UserRepository.java
@@ -2,12 +2,8 @@ package com.xiao.users.repository;
 
 import com.xiao.users.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
 }
