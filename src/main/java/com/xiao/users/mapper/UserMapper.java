4871ec2c7c212a9f97a8b0a55733d40c5cb90c01
tnd <tnd.dev99@gmail.com>
2024-03-28 10:17:15 +0700

    XIAO-106: Ignore password return & change unit test

--- src/main/java/com/xiao/users/mapper/UserMapper.java
+++ src/main/java/com/xiao/users/mapper/UserMapper.java
@@ -4,10 +4,11 @@ import com.xiao.users.dto.UserDto;
 import com.xiao.users.dto.UserUpdateDto;
 import com.xiao.users.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-
+    @Mapping(ignore = true, target = "password")
     UserDto userToUserDto(User user);
 
     User userDtoToUser(UserDto userDto);
