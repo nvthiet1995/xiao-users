b58396cf13cc45370160f24fbdc7b5d09a5b157a
TND <danh.truong@southtelecom.vn>
2024-05-29 18:04:06 +0700

    XIAO-107: create description for apis

--- src/main/java/com/xiao/users/controller/UserController.java
+++ src/main/java/com/xiao/users/controller/UserController.java
@@ -5,6 +5,12 @@ import com.xiao.users.dto.ResponseDto;
 import com.xiao.users.dto.UserDto;
 import com.xiao.users.dto.UserUpdateDto;
 import com.xiao.users.service.IUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "User", description = "User management APIs")
 public class UserController {
 
     private final IUserService iUserService;
@@ -25,6 +32,15 @@ public class UserController {
         this.iUserService = iUserService;
     }
 
+    @Operation(
+            summary = "Create a new user",
+            description = "Create an account with a role to use in the XIAO system",
+            tags = {"user", "post"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @PostMapping
     public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody UserDto userDto) {
         iUserService.createUser(userDto);
@@ -34,6 +50,15 @@ public class UserController {
                 .body(new ResponseDto(UserConstants.STATUS_201, UserConstants.MESSAGE_201));
     }
 
+    @Operation(
+            summary = "Get a user",
+            description = "Get a user by Id",
+            tags = {"user", "get"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = UserDto.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserDto> findUserById(@PathVariable("id") Long userId) {
         UserDto userDto = iUserService.findUserById(userId);
@@ -42,6 +67,15 @@ public class UserController {
                 .body(userDto);
     }
 
+    @Operation(
+            summary = "Get a user",
+            description = "Get a user by Id",
+            tags = {"user", "get"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = UserDto.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @GetMapping
     public ResponseEntity<Page<UserDto>> findAllUser(
             @RequestParam(defaultValue = "0") int pages,
