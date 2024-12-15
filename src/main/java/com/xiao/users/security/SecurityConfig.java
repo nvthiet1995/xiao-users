0b8aa355c5c7d80a337c7805446ba7e9df9e477b
DESKTOP-QQHFIHU\ThietNguyen <nvthiet.it.95@gmail.com>
2024-12-15 16:33:47 +0700

    XIAO-113 Update security config to enable oauth2

--- src/main/java/com/xiao/users/security/SecurityConfig.java
+++ src/main/java/com/xiao/users/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.xiao.users.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,11 +13,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/swagger-ui/**").permitAll()
                         .requestMatchers("/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 );
+        // enable oauth2 to verify token
+        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
         return http.build();
     }
 }
