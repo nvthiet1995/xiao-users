b75e18925f16506efd2164a5b97c537329cf0511
TND <tnd.dev99@gmail.com>
2024-12-30 16:09:27 +0700

    XIAO-115: remove reduntdant

--- src/main/java/com/xiao/users/security/SecurityConfig.java
+++ src/main/java/com/xiao/users/security/SecurityConfig.java
@@ -22,7 +22,6 @@ public class SecurityConfig {
 
         // enable oauth2 to verify token
         http.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
-
         return http.build();
     }
 }
