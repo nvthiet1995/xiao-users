24319e3170d0fb4443b5e2083a8313a0a8d527e8
TND <tnd.dev99@gmail.com>
2024-12-30 16:40:52 +0700

    XIAO-122: add rule check empty role & change integration test

--- src/test/java/com/xiao/users/util/UserUtil.java
+++ src/test/java/com/xiao/users/util/UserUtil.java
@@ -1,24 +1,34 @@
 package com.xiao.users.util;
 
+import com.xiao.users.dto.RoleDto;
 import com.xiao.users.dto.UserDto;
 import com.xiao.users.dto.UserUpdateDto;
 import com.xiao.users.entity.User;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class UserUtil {
 
     public static UserUpdateDto buildUserUpdateDto() {
+        Set<RoleDto> roleDtoSet = new HashSet<>();
+        roleDtoSet.add(RoleUtil.buildRoleDto());
         return UserUpdateDto.builder()
                 .username("abc_update")
                 .emailAddress("abc_update@gmail.com")
                 .password("passwordUpdate")
+                .roles(roleDtoSet)
                 .build();
     }
 
     public static UserDto buildUserDto() {
+        Set<RoleDto> roleDtoSet = new HashSet<>();
+        roleDtoSet.add(RoleUtil.buildRoleDto());
         return UserDto.builder()
                 .username("abc")
                 .emailAddress("abc@gmail.com")
                 .password("abcPassword")
+                .roles(roleDtoSet)
                 .build();
     }
 
